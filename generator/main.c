33d42b90a17b91c38333644fe208ad4f56848de2
headless-axolotl <martin.kostadinov05@gmail.com>
2026-07-16 13:25:36 +0300

    wrote the generation of the bwt and the lcp array; wrote the procedures and commands in the rust project to transform them into a more useful format

--- generator/main.c
+++ generator/main.c
@@ -1,93 +1,115 @@
 
+#include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-#include "libsais.h"
+#include "libsais64.h"
+
+#if defined(__clang__)
+// Shut up clangd.
+extern int ftruncate(int,int);
+#endif
 
 typedef uint8_t u8;
 typedef uint64_t u64;
-typedef int32_t i32;
+typedef int64_t i64;
 
 #define arralloc(T, n) ((T *)malloc((n) * sizeof(T)))
+#define handle_error(msg) \
+    do { perror((msg)); exit(EXIT_FAILURE); } while (0)
 
-void tiny_test();
+void generate_from_file(int argc, char *argv[]);
 
-int main() {
-    tiny_test();
+int main(int argc, char *argv[]) {
+    generate_from_file(argc, argv);
     return 0;
 }
 
-void tiny_test() {
-    const u8 *example = (u8 *)"$CCACC$GGA$GCAT$GCA$";
-
-    i32 len = strlen((char *)example);
-
-    u8 *string = arralloc(u8, len);
-    memcpy(string, example, len);
-
-    i32 *sa = arralloc(i32, len);
-    i32 *plcp = arralloc(i32, len);
-    i32 *lcp = arralloc(i32, len);
-    u8 *bwt = arralloc(u8, len);
-    i32 *temp = arralloc(i32, len);
-    i32 freq[256];
-
-    i32 err = libsais(string, sa, len, 0, NULL);
-    if (err < 0) {
-        printf("sa error\n");
-        goto defer;
+void generate_from_file(int argc, char *argv[]) {
+    if (argc < 4) {
+        fprintf(stderr, "program <INPUT_PATH> <BWT_OUTPUT_PATH> <LCP_OUTPUT_PATH>");
+        return;
     }
 
-    err = libsais_plcp(string, sa, plcp, len);
-    if (err < 0) {
-        printf("plcp error\n");
-        goto defer;
-    }
+    int status;
+    char *input_file_path = argv[1];
+    char *bwt_path = argv[2];
+    char *lcp_path = argv[3];
+    struct stat file_information = {0};
 
-    err = libsais_lcp(plcp, sa, lcp, len);
-    if (err < 0) {
-        printf("lcp error\n");
-        goto defer;
-    }
- 
-    i32 primary = libsais_bwt(string, bwt, temp, len, 0, freq);
+    int input_fd = open(input_file_path, O_RDONLY);
+    if (input_fd < 0) handle_error("open input_file_path");
+
+    status = fstat(input_fd, &file_information);
+    if (status < 0) handle_error("fstat");
+
+    i64 len = file_information.st_size;
+    u8 *string = (u8 *)mmap(NULL, len, PROT_READ, MAP_PRIVATE, input_fd, 0);
+    if (string == NULL) handle_error("mmap input_fd");
+
+    int bwt_fd = open(bwt_path, O_RDWR | O_CREAT, 0644);
+    if (bwt_fd < 0) handle_error("open bwt_path");
+    status = ftruncate(bwt_fd, len);
+    if (status < 0) handle_error("ftruncate bwt_fd"); 
+    u8 *bwt = (u8 *)mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, bwt_fd, 0);
+    memset(bwt, 0, len);
+    if (bwt == NULL) handle_error("mmap bwt_fd");
+
+    int lcp_fd = open(lcp_path, O_RDWR | O_CREAT, 0644);
+    if (lcp_fd < 0) handle_error("open lcp_path");
+    i64 lcp_len = len * sizeof(i64);
+    status = ftruncate(lcp_fd, lcp_len);
+    if (status < 0) handle_error("ftruncate lcp_fd"); 
+    i64 *lcp = (i64 *)mmap(NULL, lcp_len, PROT_READ | PROT_WRITE, MAP_SHARED, lcp_fd, 0);
+    if (lcp == NULL) handle_error("mmap lcp_fd");
+    
+    i64 freq[256];
+
+    // Use the lcp file mapped memory for the temporary array.
+    i64 primary = libsais64_bwt(string, bwt, lcp, len, 0, freq);
     if (primary < 0) {
         printf("bwt error\n");
-        goto defer;
     }
-
-    printf("%d\n", primary);
-    printf("%.*s\n", len, bwt);
-
-    {
-        printf("%2d", sa[0]);
-        for (int i = 1; i < len; ++i) {
-            printf(", %2d", sa[i]);
-        }
-        printf("\n");
-    }
-    {
-        printf("%2d", lcp[0]);
-        for (int i = 1; i < len; ++i) {
-            printf(", %2d", lcp[i]);
-        }
-        printf("\n");
-    }
+    printf("bwt primary: [%ld]\n", primary);
 
     u8 chars[] = {'$', 'A', 'C', 'G', 'T'};
     for (int i = 0; i < 5; ++i) {
-        printf("(%2d) %c: %d\n", chars[i], chars[i], freq[chars[i]]);
+        printf("(%2d) %c: %ld\n", chars[i], chars[i], freq[chars[i]]);
+    }
+    munmap(bwt, len);
+
+    i64 *sa = arralloc(i64, len);
+    i64 err = libsais64(string, sa, len, 0, NULL);
+    if (err < 0) {
+        printf("sa error\n");
+        free(sa);
+        munmap(string, len);
+        munmap(lcp, lcp_len);
+        return;
     }
 
-defer:
-    free(temp);
-    free(bwt);
+    i64 *plcp = arralloc(i64, len);
+    err = libsais64_plcp(string, sa, plcp, len);
+    munmap(string, len);
+    if (err < 0) {
+        printf("plcp error\n");
+        free(sa);
+        free(plcp);
+        munmap(lcp, lcp_len);
+        return;
+    }
+
+    err = libsais64_lcp(plcp, sa, lcp, len);
     free(sa);
     free(plcp);
-    free(lcp);
-    free(string);
-    return;
+    munmap(lcp, lcp_len);
+    if (err < 0) {
+        printf("lcp error\n");
+    }
 }
 
--- transform/src/main.rs
+++ transform/src/main.rs
@@ -2,6 +2,15 @@
 
 use clap::{Parser, Subcommand};
 use jseqio::reader::*;
+use sbwt::SeqStream;
+
+use simple_sds_sbwt::ops::{Rank, Select};
+use simple_sds_sbwt::raw_vector::{RawVector, AccessRaw};
+use simple_sds_sbwt::bit_vector::BitVector;
+use simple_sds_sbwt::serialize::Serialize;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write, Read};
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -15,48 +24,219 @@ struct Cli {
 #[command(about, long_about)]
 enum Commands {
     /// Concatenate the input sequences stored in files, given in a list file.
-    #[command(name="concat")]
+    ///
+    /// Output file extension should be ".concat".
+    #[command(name = "concat")]
     Concatenate {
-        #[arg(short='l')]
+        /// A file with filenames on separate lines.
+        #[arg(short = 'l')]
         file_list_path: PathBuf,
 
-        #[arg(short='d')]
+        /// The directory in which the files are located.
+        ///
+        /// By default searches for the files in the current working directory.
+        #[arg(short = 'd')]
         directory_with_sequence_files: Option<PathBuf>,
 
-        #[arg(short='o')]
+        /// Output path for the concatenated input sequences. Defaults to "./result.concat".
+        #[arg(short = 'o')]
         output_path: Option<PathBuf>,
     },
     /// Truncate numbers from LCP array.
-    #[command(name="trunc")]
-    TruncateLcp,
+    ///
+    /// Input file extension should be ".lcp". Output file extensions should be ".lcpt".
+    ///
+    /// The integers in the output file are little endian.
+    #[command(name = "trunc")]
+    TruncateLcp {
+        /// The path to the input LCP.
+        #[arg(short = 'i')]
+        input_path: PathBuf,
+
+        /// The path to the output LCP. Defaults to "./result.lcpt".
+        #[arg(short = 'o')]
+        output_path: Option<PathBuf>,
+
+        /// The upper bound to truncate the values in the LCP.
+        #[arg(short = 'k')]
+        max_k: u32,
+
+        /// Treat the integers in the input file as big endian.
+        #[arg(default_value_t = false, long = "big_endian")]
+        big_endian: bool,
+    },
     /// Make bitvectors from BWT.
-    #[command(name="bwt")]
-    BwtBitvectors,
+    ///
+    /// Input file extension should be ".bwt". Output file extensions should be ".bwtb".
+    #[command(name = "bwt")]
+    BwtBitVectors {
+        /// The path to the input BWT. The expected characters are {'$', 'A', 'C', 'G', 'T'}.
+        #[arg(short = 'i')]
+        input_path: PathBuf,
+
+        /// The path to the output LCP. Defaults to "./result.bwtb".
+        #[arg(short = 'o')]
+        output_path: Option<PathBuf>,
+    }
 }
 
 fn main() {
     let Cli { command } = Cli::parse();
-    // todo(mk)...
+    use Commands::*;
+    match command {
+        Concatenate {
+            file_list_path,
+            directory_with_sequence_files,
+            output_path,
+        } => {
+            concatenate(file_list_path, directory_with_sequence_files, output_path).unwrap();
+        }
+        TruncateLcp {
+            input_path,
+            output_path,
+            max_k,
+            big_endian,
+        } => {
+            if !big_endian {
+                truncate_lcp::<false>(input_path, output_path, max_k).unwrap();
+            } else {
+                truncate_lcp::<true>(input_path, output_path, max_k).unwrap();
+            }
+        }
+        BwtBitVectors {
+            input_path,
+            output_path
+        } => {
+            bwt_bit_vectors(input_path, output_path).unwrap();
+        },
+    };
+}
+
+fn concatenate(
+    file_list_path: PathBuf,
+    directory_with_sequence_files: Option<PathBuf>,
+    output_path: Option<PathBuf>,
+) -> std::io::Result<()> {
+    let input_sequences = read_lines(&file_list_path)?;
+    if let Some(dir) = directory_with_sequence_files {
+        std::env::set_current_dir(dir)?;
+    }
+    let output_path = match output_path {
+        Some(value) => value,
+        None => PathBuf::from("./result.concat"),
+    };
+    let output_file = File::create(output_path)?;
+    let mut output_writer = BufWriter::new(output_file);
+    let mut sequence_reader = SeqReader::new(&input_sequences);
+    write_concatenation(sequence_reader, &mut output_writer)?;
+    Ok(())
+}
+
+pub fn write_concatenation<SS: SeqStream + Send, W: std::io::Write>(mut stream: SS, output: &mut W) -> std::io::Result<()> {
+    write!(output, "$")?;
+    while let Some(sequence) = stream.stream_next() {
+        output.write_all(sequence)?;
+        write!(output, "$")?;
+    }
+    Ok(())
+}
+
+fn truncate_lcp<const BIG_ENDIAN: bool>(
+    input_path: PathBuf,
+    output_path: Option<PathBuf>,
+    max_k: u32,
+) -> std::io::Result<()> {
+    let input_file = File::open(input_path)?;
+    let mut input_reader = BufReader::new(input_file);
+
+    let output_path = match output_path {
+        Some(value) => value,
+        None => PathBuf::from("./result.lcpt"),
+    };
+    let output_file = File::create(output_path)?;
+    let mut output_writer = BufWriter::new(output_file);
+
+    let mut bytes = [0_u8; size_of::<u64>()];
+    let k_bit_width = u32::BITS - max_k.leading_zeros();
+    let output_byte_count = k_bit_width.div_ceil(u8::BITS) as usize;
+
+    while input_reader.read_exact(&mut bytes).is_ok() {
+        let number = if BIG_ENDIAN {
+            u64::from_be_bytes(bytes)
+        } else {
+            u64::from_le_bytes(bytes)
+        };
+        let truncated_number = number.min(max_k as u64);
+        let result_bytes = &truncated_number.to_le_bytes()[..output_byte_count];
+        output_writer.write_all(result_bytes)?;
+    }
+    Ok(())
+}
+
+fn bwt_bit_vectors(
+    input_path: PathBuf,
+    output_path: Option<PathBuf>,
+) -> std::io::Result<()> {
+    assert_eq!(usize::BITS, u64::BITS, "Use a 64-bit machine pretty please.");
+    let input_file = File::open(input_path)?;
+    let metadata = input_file.metadata()?;
+    let mut input_reader = BufReader::new(input_file);
+    let len = metadata.len() as usize;
+
+    let output_path = match output_path {
+        Some(value) => value,
+        None => PathBuf::from("./result.bwtb"),
+    };
+    let output_file = File::create(output_path)?;
+    let mut output_writer = BufWriter::new(output_file);
+
+    let mut raw_vectors = [
+        RawVector::with_len(len, false), // $
+        RawVector::with_len(len, false), // A
+        RawVector::with_len(len, false), // C
+        RawVector::with_len(len, false), // G
+        RawVector::with_len(len, false), // T
+    ];
+
+    let mut byte = [0_u8];
+    let mut index: usize = 0;
+    while input_reader.read_exact(&mut byte).is_ok() {
+        match byte[0] {
+            b'$' => raw_vectors[0].set_bit(index, true),
+            b'A' => raw_vectors[1].set_bit(index, true),
+            b'C' => raw_vectors[2].set_bit(index, true),
+            b'G' => raw_vectors[3].set_bit(index, true),
+            b'T' => raw_vectors[4].set_bit(index, true),
+            _ => {}
+        };
+        index += 1;
+    }
+
+    let mut bit_vectors = raw_vectors.into_iter().map(BitVector::from).collect::<Vec<_>>();
+    bit_vectors.iter_mut().for_each(|vector| {
+        vector.enable_rank();
+        vector.enable_select();
+    });
+
+    for bit_vector in bit_vectors {
+        bit_vector.serialize(&mut output_writer)?;
+    }
+    Ok(())
 }
 
 //
 // Code plagiarised from the swbt crates.
 // {
 //
-trait SeqStream {
-    fn stream_next(&mut self) -> Option<&[u8]>;
-}
-
-fn read_lines(path: &PathBuf) -> Vec<PathBuf> {
-    use std::io::BufRead;
-    let file = std::fs::File::open(path).unwrap();
+fn read_lines(path: &PathBuf) -> std::io::Result<Vec<PathBuf>> {
+    let file = std::fs::File::open(path)?;
     let reader = std::io::BufReader::new(file);
     let mut paths = Vec::<PathBuf>::new();
     for line in reader.lines() {
-        let line = line.unwrap();
+        let line = line?;
         paths.push(PathBuf::from(line));
     }
-    paths
+    Ok(paths)
 }
 
 struct SeqReader<'a> {
@@ -77,7 +257,7 @@ impl<'a> SeqReader<'a> {
     }
 }
 
-impl SeqStream for SeqReader<'_> {
+impl sbwt::SeqStream for SeqReader<'_> {
     fn stream_next(&mut self) -> Option<&[u8]> {
         loop {
             if let Some(current) = &mut self.current {
